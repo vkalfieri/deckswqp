ea2d209db83d35e177929da2cdce5e0070bbbab8
vkalfieri <phantom_11@hotmail.com>
2017-05-09 14:40:43 +1000

    added extra functionality and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  devise_for :installs
   resources :listings
   resources :subcategories
   resources :deliveries
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503062603) do
+ActiveRecord::Schema.define(version: 20170509010930) do
 
   create_table "categories", force: :cascade do |t|
     t.integer  "category_id"
@@ -29,6 +29,23 @@ ActiveRecord::Schema.define(version: 20170503062603) do
     t.datetime "updated_at",    null: false
     t.index ["listing_id_id"], name: "index_deliveries_on_listing_id_id"
     t.index ["user_id_id"], name: "index_deliveries_on_user_id_id"
+  end
+
+  create_table "installs", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
   end
 
   create_table "listings", force: :cascade do |t|
@@ -85,6 +102,23 @@ ActiveRecord::Schema.define(version: 20170503062603) do
     t.integer  "buyer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
